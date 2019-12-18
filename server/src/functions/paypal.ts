686ba6d809cd9fcaf154a4d46dd2d303c992e10b
Steven McKeown <steven.j.mckeown@gmail.com>
2019-12-17 19:49:45 -0500

    PayPal IPN webhook
    Added typescript to the server
    I could not convert index.js due to a bug reported an hour ago:
    https://github.com/firebase/firebase-admin-node/issues/734
    
    Added firebase.json to hopefully start the webhook according to the
    documentation.
    
    Receive the IPN payload and validate that the payload it real
    
    All untested, Ido not have a firebase account

---
+++ server/src/functions/paypal.ts
@@ -0,0 +1,47 @@
+import * as functions from 'firebase-functions'
+import PayPalIpn from 'paypal-ipn-types'
+import * as rp from 'request-promise'
+
+/**
+ * Verify the payload recieved with PayPal to ensure that it is an authentic message
+ * 
+ * @param ipnData The payload received for PayPal IPN via the webhook
+ * @returns {boolean} true if message is valid and can be processed, false if fake
+ *  
+ * TODO: use the functions.config() to either point to sandbox.paypal.com for test purposes or ipnpb.paypal.com for live
+ */
+async function isValid(ipnData: PayPalIpn) {
+  const validationResponse = await rp({
+    method: 'POST',
+    uri: 'https://ipnpb.paypal.com/cgi-bin/webscr', // functions.config().paypal.ipn_url,
+    formData: {
+      cmd:'_notify-validate',
+      ...ipnData
+    }
+  })
+  return validationResponse === 'VERIFIED' // other return is INVALID. If it is INVALID it is probably spoofed
+}
+
+export const paypalIPN = functions.https.onRequest(async (request, reply) => {
+  const ipnData = request.body as PayPalIpn
+  
+  if (isValid(ipnData)) {
+    switch(ipnData.txn_type) {
+      case 'recurring_payment': {
+
+      }
+      break;
+      case 'web_accept': {
+        // generate receipt
+      }
+      break;
+      default: {
+        console.log(`received transaction of type ${ipnData.txn_type} and did not have an action to take`)
+      }
+    }
+    reply.sendStatus(200)
+  } else {
+    console.error('IPN verification failed')
+    console.error(ipnData)
+  }
+})
