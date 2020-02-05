ab56f5227ae1b47d7a88f79c3423fedd6b1f43a6
Gabriel Lemire <gabriel.lemire@gmail.com>
2020-02-04 22:21:59 -0500

    Temp sending email directly after generating PDF

--- server/src/functions/pubsub/pdf-receipt.ts
+++ server/src/functions/pubsub/pdf-receipt.ts
@@ -1,3 +1,4 @@
+import { EOL } from 'os'
 import { PubSubHandler } from '../../utils/pubsub-function'
 import { readJsonMessage } from '../../utils/pubsub'
 import { logger } from '../../utils/logging'
@@ -13,6 +14,7 @@ import { getFileProvider } from '../../providers/file'
 import { HandlebarsError } from '../../errors/HandlebarsError'
 import { donationActivityService } from '../../services/donation-activity-service'
 import { FileProvider } from '../../providers/file/FileProvider'
+import { sendEmail } from '../../providers/email/SendGridEmailProvider'
 
 export interface GeneratePdfCommand {
   donationId: string
@@ -22,6 +24,7 @@ export interface GeneratePdfCommand {
 /**
  * Generates and saves a PDF based on a PubSubMessage
  * @param message PubSubMessage triggering the PDF generation
+ * @todo Implement a soft retry
  */
 export const pdf: PubSubHandler = async message => {
   const command = readJsonMessage<GeneratePdfCommand>(message)
@@ -34,14 +37,37 @@ export const pdf: PubSubHandler = async message => {
   const filename = `receipt_${command.donationId}_${receiptContent.receiptNumber}.pdf`
   await fileProvider.saveDocument(filename, pdf)
 
-  await donationActivityService.addDocument(
+  const donation = await donationActivityService.addDocument(
     command.donationId,
     receiptContent.receiptNumber,
     filename,
     `Fiscal receipt for ${receiptContent.fiscalYear}`
   )
 
-  // TODO: Queue email transmission (as necessary)
+  const content = [
+    'Thank you for you donation to our organization. This is much appreciated.',
+    `You will find attached to this email your fiscal receipt for the year ${donation.fiscalYear}.`,
+    'Thank you again for your donation',
+  ].reduce((acc, current) => {
+    if (acc) {
+      acc = `${acc}${EOL}${EOL}`
+    }
+
+    acc = `${acc}${current}`
+    return acc
+  }, '')
+
+  // TODO: Queue email transmission in its own job
+  logger.info('Sending receipt by email')
+  await sendEmail({
+    to: donation.donor.email || '',
+    content,
+    contentType: 'text',
+    subject: 'Thank you for your donation',
+    attachments: [
+      { name: 'receipt.pdf', contentType: 'application/pdf', data: pdf },
+    ],
+  })
 }
 
 interface ReceiptContent {
