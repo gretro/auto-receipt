42c5381e46c9ee2e230698f4621d8e16d6321d31
Gabriel Lemire <gabriel.lemire@gmail.com>
2020-09-04 15:11:54 -0400

    Temporary stage for new client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,54 @@
+import { Box, CssBaseline } from '@material-ui/core';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { Page2 } from './page2/pag2.page';
-import { Page1 } from './page1/page1.page';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { HomePage } from './home/home.page';
 import { NotFoundPage } from './not-found/not-found.page';
+import { Page1 } from './page1/page1.page';
+import { Page2 } from './page2/pag2.page';
+import { AppSidebar } from './shell/app-side-bar/app-side-bar.component';
+
+const theme = createMuiTheme();
+
+const useStyles = makeStyles(() => ({
+  appContainer: {
+    height: '100vh',
+    width: '100vw',
+    display: 'grid',
+  },
+}));
 
 export const App: React.FC = () => {
+  const styles = useStyles();
+
   return (
-    <>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/page1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/page2">Page 2</Link>
-            </li>
-            <li>
-              <Link to="/patate">Not Found</Link>
-            </li>
-          </ul>
-        </nav>
+    <Box className={styles.appContainer}>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <nav>
+            <AppSidebar />
+          </nav>
 
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
 
-          <Route path="/page1">
-            <Page1 />
-          </Route>
+            <Route path="/page1">
+              <Page1 />
+            </Route>
 
-          <Route path="/page2">
-            <Page2 />
-          </Route>
+            <Route path="/page2">
+              <Page2 />
+            </Route>
 
-          <Route path="*">
-            <NotFoundPage />
-          </Route>
-        </Switch>
-      </Router>
-    </>
+            <Route path="*">
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </Box>
   );
 };
-
-export default App;
--- client/src/home/home.page.tsx
+++ client/src/home/home.page.tsx
@@ -1,9 +1,10 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 
 export const HomePage: React.FC = () => {
   return (
     <>
-      <h1>Welcome to the Home page</h1>
+      <Typography variant="h1">Home page</Typography>
     </>
   );
 };
