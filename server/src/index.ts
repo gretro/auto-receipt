324136f364ed93274cf49db6aaf1cca105b11d5e
Gabriel Lemire <gabriel.lemire@gmail.com>
2020-09-26 20:30:53 -0400

    Fix deployment (missing email pubsub fn + missing config)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
-export { paypalIpn } from './functions/http/paypal-ipn'
+// HTML functions
+export { launchBulkImport } from './functions/http/bulk-import-donations'
 export { createCheque } from './functions/http/create-cheque-donation'
 export { listDonations } from './functions/http/donation-management'
 export { generatePdfReceipt } from './functions/http/generate-pdf-receipt'
-export { launchBulkImport } from './functions/http/bulk-import-donations'
-
-export { pdf } from './functions/pubsub/pdf-receipt'
+export { paypalIpn } from './functions/http/paypal-ipn'
+// Pubsub functions
 export { bulkImport } from './functions/pubsub/bulk-import'
+export { email } from './functions/pubsub/email'
+export { pdf } from './functions/pubsub/pdf-receipt'
