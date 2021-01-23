641f74bf0277b04f90077d00b7ac3db552612664
Gabriel Lemire <gabriel.lemire@gmail.com>
2021-01-23 17:08:06 -0500

    Remove SendGrid dependency

--- server/src/providers/email/index.ts
+++ server/src/providers/email/index.ts
@@ -1,16 +1,10 @@
 import * as config from 'config'
-import { InvalidConfigurationError } from '../../errors/InvalidConfigurationError'
 import { EmailProvider } from './EmailProvider'
 import { fakeEmailProvider } from './FakeEmailProvider'
-import {
-  sendGridEmailProviderFactory,
-  SendGridOptions,
-} from './SendGridEmailProvider'
 import { smtpEmailProviderFactory, SmtpOptions } from './SmtpEmailProvider'
 
 interface EmailProviderConfig {
-  provider: 'sendGrid' | 'fake' | 'smtp'
-  sendGrid?: SendGridOptions | null
+  provider: 'fake' | 'smtp'
   smtp?: SmtpOptions | null
 }
 
@@ -18,16 +12,6 @@ export function getEmailProvider(): EmailProvider {
   const emailProviderConfig = config.get<EmailProviderConfig>('providers.email')
 
   switch (emailProviderConfig.provider) {
-    case 'sendGrid': {
-      if (!emailProviderConfig.sendGrid) {
-        throw new InvalidConfigurationError(
-          'Could not find configuration for sendGrid email provider [providers.email.sendGrid]'
-        )
-      }
-
-      return sendGridEmailProviderFactory(emailProviderConfig.sendGrid)
-    }
-
     case 'smtp':
       return smtpEmailProviderFactory(emailProviderConfig.smtp)
 
